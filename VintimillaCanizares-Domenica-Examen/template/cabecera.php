6ea05c85a8f718d6ef8e57f16c785c6b7ab196f1
Domenica Vintimilla <49033368+DomenicaC@users.noreply.github.com>
2020-12-06 11:51:38 -0500

    hacer los pedidos

--- VintimillaCanizares-Domenica-Examen/template/cabecera.php
+++ VintimillaCanizares-Domenica-Examen/template/cabecera.php
@@ -12,15 +12,6 @@
             /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
         }
 
-        @font-face {
-            font-family: indie;
-            src: url(../IndieFlower-Regular.ttf);
-        }
-
-        * {
-            font-family: 'indie';
-        }
-
         h1 {
             text-align: center;
         }
@@ -34,6 +25,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
